a665431b0405ab92fd4546c216087613396f530d
wangonya <kwangonya@gmail.com>
2019-09-26 12:07:08 +0300

    solve cors issue

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from fetch_data import (firebase_db, currentYear, currentMonth)
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
